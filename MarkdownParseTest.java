7cae7b5d2ed1bd7c46ea91e3f355e2756a42ac5d
Steven Tran <97551211+stevendtran@users.noreply.github.com>
2022-04-21 11:02:56 -0700

    1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,31 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testTestFile() throws IOException{
+        String fileName = Files.readString(Path.of("/Users/steve/Documents/School Work/Spring 22/CSE 15L/Lab 3/test-file.md"));
+
+        ArrayList<String> listOfLinks = new ArrayList<>();
+        listOfLinks = MarkdownParse.getLinks(fileName);
+
+        List<String> expectedLinks = new ArrayList<String>();
+        expectedLinks = List.of("https://something.com", "some-thing.html");
+
+        assertEquals(expectedLinks, listOfLinks);
+
     }
 }
 
